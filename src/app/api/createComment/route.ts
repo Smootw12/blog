bf78bb2cb653b5e2c725cee5853635ffd7211f8a
Daniele Di Pinto <danieledipinto.c@gmail.com>
2023-10-31 23:45:39 +0100

    ce la puoi fare campione

--- src/app/api/createComment/route.ts
+++ src/app/api/createComment/route.ts
@@ -3,18 +3,18 @@ import { Comment } from "@/util/types";
 
 import { createClient } from "next-sanity";
 
-const client = createClient({
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-12",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  token: process.env.SANITY_API_TOKEN,
-});
-
 interface request extends Comment {
   _id: string;
 }
 
 export async function POST(req: NextRequest) {
+  const client = createClient({
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-12",
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    token: process.env.SANITY_API_TOKEN,
+  });
+
   const { _id, body, email, name, rating } = (await req.json()) as request;
   try {
     await client.create({
