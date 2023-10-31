db963b09088105eac7d1fcb69ad54452d32b3603
Daniele Di Pinto <danieledipinto.c@gmail.com>
2023-11-01 00:57:09 +0100

    stavolta ce l'ho fatta davvero

--- src/app/api/createComment/route.ts
+++ src/app/api/createComment/route.ts
@@ -3,7 +3,7 @@ import { Comment } from "@/util/types";
 
 import { createClient } from "next-sanity";
 
-export const client = createClient({
+const client = createClient({
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-12",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
